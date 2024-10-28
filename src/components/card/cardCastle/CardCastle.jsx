092d427c77be02bb8ac9b51ea25f61695249bfd5
terence planchestainer <t.planchestainer@stag.technofuturtic.education>
2024-10-28 16:44:17 +0100

    cht

--- src/components/card/cardCastle/CardCastle.jsx
+++ src/components/card/cardCastle/CardCastle.jsx
@@ -1,48 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState} from 'react';
 import './CardCastle.css';
 
-import logoDistance from '../../../assets/images/height.png'
-import logoDirection from '../../../assets/images/right-arrow.png'
-
-function CardCastle({ name, images, hauteur, longeur, largeur, genre }) {
-    const [currentImageIndex, setCurrentImageIndex] = useState(0);
-
-    const handleNext = () => {
-        setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
-    };
-
-    const handlePrev = () => {
-        setCurrentImageIndex((prevIndex) =>
-            prevIndex === 0 ? images.length - 1 : prevIndex - 1
-        );
-    };
+function CardCastle({ name, images, description }) {
+    const [isHovered, setIsHovered] = useState(false);
 
     return (
-        <div className='containerCard'>
-            <div className='titleContainer'>
+        <div
+            className={`containerCard ${isHovered ? 'hovered' : ''}`}
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+        >
+            <div className="img-container">
+                <img src={images[0]} alt={name} />
+            </div>
+            <div className='description'>
                 <h3>{name}</h3>
-            </div>
-            <div className='containerImg'>
-                <button onClick={handlePrev}><img src={logoDirection} alt=""/></button>
-                <img src={images[currentImageIndex]} alt={name} className='imageCurent' />
-                <div></div>
-                <button onClick={handleNext}><img src={logoDirection} alt="" /></button>
-            </div>
-            <div className='indication'>
-                <div>
-                    <img src={logoDistance} alt="" className='hauteur'/> 
-                    <p>{hauteur}</p>   
-                </div>
-                <div>
-                    <img src={logoDistance} alt="" className='longeur'/>
-                    <p>{longeur}</p>
-                </div>
-                <div>
-                    <img src={logoDistance} alt="" className='largeur'/>
-                    <p>{largeur}</p>
-                </div>
-                
-                
+                <p>{description}</p>
             </div>
         </div>
     );
--- src/pages/InflatableCastle/InflatableCastle.jsx
+++ src/pages/InflatableCastle/InflatableCastle.jsx
@@ -7,30 +7,84 @@ import logoCastleLogo from '../../assets/images/logos/iconBouncyCastle/bouncy-ca
 import './InflatableCastle.css'
 
 function InflatableCastle() {
+    // Filtrer les données pour afficher uniquement les éléments avec genre 'fille'
+    const filteredCastlesGirle = castelData.filter(castle => castle.genre === 'fille');
+    const filteredCastlesBoy = castelData.filter(castle => castle.genre === 'garcon');
+    const filteredCastlesMixte = castelData.filter(castle => castle.genre === 'mixte');
+
     return (
         <div>
+
             <TopOfPage
                 src={logoCastleLogo}
                 alt="Placeholder Image"
                 title="Chateaux Gonflable"
                 message="Nos gammes de châteaux gonflables"
             />
+
             <div className='cards-container'>
-                {castelData.map((castle) => (
-                    <CardCastle
-                        key={castle.id}
-                        name={castle.name}
-                        images={castle.images}
-                        hauteur={castle.hauteur}
-                        longeur={castle.longeur}
-                        largeur={castle.largeur}
-                        genre={castle.genre}
-                        className='card'
-                    />
-                ))}
+                <div className='containerGirl'>
+                    <h3>Châteaux pour Filles</h3> {/* Ajout du titre */}
+                </div >
+                <div className='display'>
+
+                    {filteredCastlesGirle.map((castle) => (
+                        <CardCastle
+                            key={castle.id}
+                            name={castle.name}
+                            images={castle.images}
+                            description={castle.description}
+                            // hauteur={castle.hauteur}
+                            // longeur={castle.longeur}
+                            // largeur={castle.largeur}
+                            genre={castle.genre}
+                            className='card'
+                        />
+                    ))}
+                </div>
+                <div className='containerBoys'>
+                    <h3>Châteaux pour Garçons</h3> {/* Ajout du titre */}
+                </div>
+
+                <div className='display'>
+
+                    {filteredCastlesBoy.map((castle) => (
+                        <CardCastle
+                            key={castle.id}
+                            name={castle.name}
+                            images={castle.images}
+                            description={castle.description}
+                            // hauteur={castle.hauteur}
+                            // longeur={castle.longeur}
+                            // largeur={castle.largeur}
+                            genre={castle.genre}
+                            className='card'
+                        />
+                    ))}
+                </div>
+                <div className='containerMixte'>
+                    <h3>Châteaux Mixtes</h3> {/* Ajout du titre */}
+                </div>
+
+                <div className='display'>
+
+                    {filteredCastlesMixte.map((castle) => (
+                        <CardCastle
+                            key={castle.id}
+                            name={castle.name}
+                            images={castle.images}
+                            description={castle.description}
+                            // hauteur={castle.hauteur}
+                            // longeur={castle.longeur}
+                            // largeur={castle.largeur}
+                            genre={castle.genre}
+                            className='card'
+                        />
+                    ))}
+                </div>
             </div>
         </div>
     );
 }
-
 export default InflatableCastle;
+
